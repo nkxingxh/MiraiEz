8d0c6f035e1a95e10a3ad6ed81d09619553b74d0
NKXingXh <nkxingxh@nkxingxh.top>
2023-03-02 16:08:20 +0800

    优化 curl 相关函数封装

--- core/curl.php
+++ core/curl.php
@@ -11,184 +11,51 @@
 
 function CurlGET($url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
-    $header = is_array($header) ? $header : array($header);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    
-    #返回数据不直接显示
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($curl, CURLOPT_TIMEOUT, 10);
-
-    //适配 gzip 压缩
-    curl_setopt($curl, CURLOPT_ENCODING, 'gzip, deflate');
-
-    if (!empty($setopt) && is_array($setopt)) {
-        foreach ($setopt as $value) {
-            curl_setopt($curl, $value[0], $value[1]);
-        }
-    }
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+    return Curl(null, $url, $cookie, $referer, $header, $setopt, $UserAgent);
 }
 
 function CurlPOST($payload, $url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
-    if (MIRAIEZ_LOGGING_LEVEL == 1) {
-        writeLog('URL: ' . $url, __FUNCTION__, 'curl', 1);
-        writeLog('Payload: ' . (is_array($payload) ? ('(Array) ' . json_encode($payload, JSON_UNESCAPED_UNICODE)) : $payload), __FUNCTION__, 'curl', 1);
-    }
-    $header = is_array($header) ? $header : array($header);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    #返回数据不直接显示
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_POST, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-
-    //适配 gzip 压缩
-    curl_setopt($curl, CURLOPT_ENCODING, 'gzip, deflate');
-
-    if (!empty($setopt) && is_array($setopt)) {
-        foreach ($setopt as $value) {
-            curl_setopt($curl, $value[0], $value[1]);
-        }
-    }
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    writeLog('Resp: ' . $response, __FUNCTION__, 'curl', 1);
-    return $response;
+    $setopt[] = [CURLOPT_POST, 1];
+    return Curl($payload, $url, $cookie, $referer, $header, $setopt, $UserAgent);
 }
 
 function CurlPUT($payload, $url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
-    $header = is_array($header) ? $header : array($header);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    #返回数据不直接显示
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-    if (!empty($setopt) && is_array($setopt)) {
-        foreach ($setopt as $value) {
-            curl_setopt($curl, $value[0], $value[1]);
-        }
-    }
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+    $setopt[] = [CURLOPT_CUSTOMREQUEST, "PUT"];
+    return Curl($payload, $url, $cookie, $referer, $header, $setopt, $UserAgent);
 }
 
 function CurlPATCH($payload, $url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
-    $header = is_array($header) ? $header : array($header);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    #返回数据不直接显示
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PATCH");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-    if (!empty($setopt) && is_array($setopt)) {
-        foreach ($setopt as $value) {
-            curl_setopt($curl, $value[0], $value[1]);
-        }
-    }
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+    $setopt[] = [CURLOPT_CUSTOMREQUEST, "PATCH"];
+    return Curl($payload, $url, $cookie, $referer, $header, $setopt, $UserAgent);
 }
 
 function CurlDELETE($payload, $url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
-    $header = is_array($header) ? $header : array($header);
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
-    #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    #返回数据不直接显示
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-    if (!empty($setopt) && is_array($setopt)) {
-        foreach ($setopt as $value) {
-            curl_setopt($curl, $value[0], $value[1]);
-        }
-    }
-    $response = curl_exec($curl);
-    curl_close($curl);
-    return $response;
+    $setopt[] = [CURLOPT_CUSTOMREQUEST, "DELETE"];
+    return Curl($payload, $url, $cookie, $referer, $header, $setopt, $UserAgent);
 }
 
-function Curl($payload, $url, $cookie = '', $referer = '', $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
+function Curl($payload, $url, $cookie = null, $referer = null, $header = array(), $setopt = array(), $UserAgent = 'MiraiEz')
 {
     $header = is_array($header) ? $header : array($header);
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-    if (!empty($header))
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
-    if ($referer != '')
-        curl_setopt($curl, CURLOPT_REFERER, $referer);
-    if ($cookie != '')
-        curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+    if (!empty($header)) curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+    if (!empty($referer)) curl_setopt($curl, CURLOPT_REFERER, $referer);
+    if (!empty($cookie)) curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+    if (!empty($payload)) curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+
     #关闭SSL
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    //curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
     #返回数据不直接显示
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+    //适配 gzip 压缩
+    curl_setopt($curl, CURLOPT_ENCODING, 'gzip, deflate');
 
     if (!empty($setopt) && is_array($setopt)) {
         foreach ($setopt as $value) {
